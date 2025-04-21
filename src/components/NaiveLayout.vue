461e61e9b4d98cf59bc895e7a22d6dae688dda38
tuongtv87 <vinhtuong.vts@gmail.com>
2025-04-21 10:35:37 +1000

    update users

--- src/components/NaiveLayout.vue
+++ src/components/NaiveLayout.vue
@@ -3,8 +3,8 @@
     <n-layout-header bordered style="height: 64px;">
       <n-space justify="space-between" align="center" style="padding: 0 20px; height: 100%;">
         <span>Header Header Header</span>
-        <n-space>
-          <n-button text @click="handleToggleTheme">
+        <n-space :size="30" align="center">
+          <n-button text @click="handleToggleTheme" style="display: flex; align-items: center; justify-content: center;">
             <template #icon>
               <n-icon>
                 <MoonOutline v-if="isLightTheme" />
@@ -12,13 +12,26 @@
               </n-icon>
             </template>
           </n-button>
-          <n-button text @click="handleLogout">
-            <template #icon>
-              <n-icon>
-                <LogOutOutline />
-              </n-icon>
-            </template>
-          </n-button>
+          
+          <!-- Menu người dùng -->
+          <n-dropdown
+            trigger="click"
+            :options="userMenuOptions"
+            @select="handleSelect"
+            placement="bottom-end"
+          >
+            <n-button text>
+              <n-space align="center">
+                <n-avatar
+                  round
+                  size="small"
+                  src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+                />
+                <span>{{ username }}</span>
+                <n-icon size="tiny"><ChevronDownOutline /></n-icon>
+              </n-space>
+            </n-button>
+          </n-dropdown>
         </n-space>
       </n-space>
     </n-layout-header>
@@ -50,17 +63,19 @@
 
 <script lang="ts">
 import type { Component, Ref } from 'vue'
-import { computed, inject } from 'vue'
+import { computed, inject, ref, onMounted } from 'vue'
 import {
   BookOutline as BookIcon,
   PersonOutline as PersonIcon,
   WineOutline as WineIcon,
   SunnyOutline,
   MoonOutline,
-  LogOutOutline
+  LogOutOutline,
+  ChevronDownOutline,
+  KeyOutline
 } from '@vicons/ionicons5'
-import { NIcon, NButton, NSpace } from 'naive-ui'
-import type { GlobalTheme } from 'naive-ui'
+import { NIcon, NButton, NSpace, NAvatar, NDropdown } from 'naive-ui'
+import type { GlobalTheme, DropdownOption } from 'naive-ui'
 import { defineComponent, h } from 'vue'
 import { useRouter, RouterView } from 'vue-router'
 
@@ -149,14 +164,36 @@ export default defineComponent({
     NIcon,
     NButton,
     NSpace,
+    NAvatar,
+    NDropdown,
     SunnyOutline,
     MoonOutline,
-    LogOutOutline
+    LogOutOutline,
+    ChevronDownOutline
   },
   setup() {
     const toggleTheme = inject<() => void>('toggleTheme')
     const currentTheme = inject<Ref<GlobalTheme>>('currentTheme')
     const router = useRouter()
+    const username = ref('')
+
+    // Lấy username từ localStorage khi component được mount
+    onMounted(() => {
+      username.value = localStorage.getItem('rememberedUsername') || 'User'
+    })
+
+    const userMenuOptions = computed<DropdownOption[]>(() => [
+      {
+        label: 'Change password',
+        key: 'change-password',
+        icon: renderIcon(KeyOutline)
+      },
+      {
+        label: 'Logout',
+        key: 'logout',
+        icon: renderIcon(LogOutOutline)
+      }
+    ])
 
     const handleToggleTheme = () => {
       if (toggleTheme && currentTheme) {
@@ -169,13 +206,25 @@ export default defineComponent({
       router.push({ name: 'Login' });
     }
 
+    const handleSelect = (key: string) => {
+      if (key === 'logout') {
+        handleLogout();
+      } else if (key === 'change-password') {
+        // Xử lý đổi mật khẩu (sẽ thực hiện sau)
+        console.log('Change password clicked');
+      }
+    }
+
     const isLightTheme = computed(() => currentTheme?.value.name === 'light')
 
     return {
       menuOptions,
       handleToggleTheme,
       handleLogout,
-      isLightTheme
+      handleSelect,
+      isLightTheme,
+      username,
+      userMenuOptions
     }
   }
 })
